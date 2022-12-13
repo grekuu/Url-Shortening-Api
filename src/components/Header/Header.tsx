f47ddeb8c35140c2eff287e0ec22b6bd9154b0a6
grekuu <gracjanprusik@gmail.com>
2022-12-13 19:10:52 +0100

    end for today

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,21 @@
 import "./Header.scss";
+import logo from "../../images/logo.svg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="header-left">
+        <img src={logo} alt="logo" />
+        <a>Features</a>
+        <a>Pricing</a>
+        <a>Resources</a>
+      </div>
+      <div className="header-right">
+        <button>Login</button>
+        <button>Sign Up</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
