881268b89bf0b8625c83adc102eb3be8a4907868
grekuu <gracjanprusik@gmail.com>
2022-12-13 18:20:26 +0100

    components included

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+
 function App() {
-  return <div>gregre</div>;
+  return (
+    <>
+      <Header></Header>
+      <Home></Home>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
