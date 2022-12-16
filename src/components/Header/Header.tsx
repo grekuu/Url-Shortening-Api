5ce3fb7dbe1005cb7ab1df1bd8ab3d6b07240a39
grekuu <gracjanprusik@gmail.com>
2022-12-16 14:53:32 +0100

    header fully responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,37 @@
 import "./Header.scss";
 import logo from "../../images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
 
 const Header = () => {
+  const [hamburgetActive, setHamburgetActive] = useState<Boolean>(false);
+
   return (
-    <div className="header">
-      <div className="header-left">
-        <img src={logo} alt="logo" />
-        <a>Features</a>
-        <a>Pricing</a>
-        <a>Resources</a>
+    <>
+      <div className="header">
+        <div className="header-left">
+          <img src={logo} alt="logo" />
+          <a className="header-href">Features</a>
+          <a className="header-href">Pricing</a>
+          <a className="header-href">Resources</a>
+        </div>
+        <div className="header-right">
+          <FaBars
+            className="hamburger"
+            onClick={() => setHamburgetActive(!hamburgetActive)}
+          ></FaBars>
+        </div>
       </div>
-      <div className="header-right">
-        <button>Login</button>
-        <button>Sign Up</button>
-      </div>
-    </div>
+      {hamburgetActive ? (
+        <div className="mobile-href">
+          <a>Features</a>
+          <a>Pricing</a>
+          <a>Resources</a>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </>
   );
 };
 
